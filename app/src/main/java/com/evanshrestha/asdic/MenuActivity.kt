c3de905191980c829f743c152ed8c91b7ba5619f
Evan Shrestha <evanshrestha@gmail.com>
2019-07-23 19:52:35 -0400

    Add RecyclerView with pulled series information

--- app/src/main/java/com/evanshrestha/asdic/MenuActivity.kt
+++ app/src/main/java/com/evanshrestha/asdic/MenuActivity.kt
@@ -1,25 +1,41 @@
 package com.evanshrestha.asdic
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.View
 import kotlinx.android.synthetic.main.activity_menu.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.json.JSONObject
+import org.jetbrains.anko.uiThread
+import org.json.JSONArray
+import java.net.URL
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    private var IP_ADDRESS : String? = null
+    private var PORT : String? = null
+    private var API_KEY : String? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.text = intent.getStringExtra("IP")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,6 +46,55 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        message.text = intent.getStringExtra("IP")
+
+        IP_ADDRESS = intent.getStringExtra("IP")
+        PORT = intent.getStringExtra("PORT")
+        API_KEY = intent.getStringExtra("API_KEY")
+
+        val seriesItems : ArrayList<Series> = ArrayList()
+
+        // Pull series info
+        doAsync {
+            val result = URL("http://" + IP_ADDRESS + ":" + PORT + "/api/series?apikey=" + API_KEY).readText()
+            try {
+                val seriesArray = JSONArray(result)
+                for (seriesIndex in 0 until seriesArray.length()) {
+                    val seriesItem : JSONObject = seriesArray.getJSONObject(seriesIndex)
+                    val seriesImages : JSONArray = seriesItem.getJSONArray("images")
+
+                    var seriesImagePosterURL : String? = null
+
+                    for (seriesImageIndex in 0 until seriesImages.length()) {
+                        val seriesImageItem = seriesImages.getJSONObject(seriesImageIndex)
+                        if (seriesImageItem.getString("coverType").equals("poster")) {
+                            seriesImagePosterURL = "http://" + IP_ADDRESS + ":" + PORT + seriesImageItem.getString("url")
+                            break
+                        }
+                    }
+
+                    val newSeries : Series = Series()
+                    newSeries.title = seriesItem.getString("title")
+                    newSeries.imageURL = seriesImagePosterURL
+                    newSeries.certification = seriesItem.getString("certification")
+                    newSeries.year = seriesItem.getString("year")
+                    seriesItems.add(newSeries)
+
+                }
+            } catch (e: Exception) {
+                uiThread {
+                    toast("Whoopsie")
+                }
+                e.printStackTrace()
+            }
+        }
+
+        linearLayoutManager = LinearLayoutManager(this)
+
+        viewAdapter = RecyclerSeriesAdapter(seriesItems)
+        var recycler = findViewById<RecyclerView>(R.id.recycle_shows)
+        recycler.apply {
+            layoutManager = linearLayoutManager
+            adapter = viewAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/evanshrestha/asdic/Series.kt
@@ -0,0 +1,8 @@
+package com.evanshrestha.asdic
+
+class Series {
+    var title : String? = null
+    var imageURL : String? = null
+    var certification : String? = null
+    var year : String? = null
+}
