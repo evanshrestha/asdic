5440d0a90ea514dc013cfa44e97b9fcc79402eb3
Evan Shrestha <evanshrestha@gmail.com>
2019-07-24 17:41:40 -0400

    Add basic click listener to RecyclerView

--- app/src/main/java/com/evanshrestha/asdic/RecyclerSeriesAdapter.kt
+++ app/src/main/java/com/evanshrestha/asdic/RecyclerSeriesAdapter.kt
@@ -1,14 +1,21 @@
 package com.evanshrestha.asdic
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.squareup.picasso.Picasso
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.toast
 
 class RecyclerSeriesAdapter(val items: ArrayList<Series>) : RecyclerView.Adapter<RecyclerSeriesAdapter.ViewHolder>() {
+
 
     class ViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(inflater.inflate(R.layout.layout_series_listitem, parent, false)) {
         private var seriesTitleTextView : TextView? = null
@@ -21,6 +28,9 @@ class RecyclerSeriesAdapter(val items: ArrayList<Series>) : RecyclerView.Adapter
             seriesCertificationTextView = itemView.findViewById(R.id.seriesCertificationText)
             seriesYearTextView = itemView.findViewById(R.id.seriesYearText)
             seriesImageView = itemView.findViewById(R.id.seriesImageView)
+            itemView.setOnClickListener {
+                Log.d("Asdic", adapterPosition.toString())
+            }
         }
 
         fun bind(series : Series) {
@@ -29,6 +39,7 @@ class RecyclerSeriesAdapter(val items: ArrayList<Series>) : RecyclerView.Adapter
             seriesYearTextView?.text = series.year
             Picasso.get().load(series.imageURL).into(seriesImageView)
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -39,7 +50,10 @@ class RecyclerSeriesAdapter(val items: ArrayList<Series>) : RecyclerView.Adapter
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val series : Series = items[position]
         holder.bind(series)
+
     }
 
     override fun getItemCount() = items.size
+
+
 }
